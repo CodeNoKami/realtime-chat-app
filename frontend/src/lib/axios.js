94561b8c6bfa430b479ebef1f9aa4621125cba82
CodeNoKami <codenokami.dev@gmail.com>
2025-06-16 07:36:24 +0800

    kill bugs

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-   baseURL: import.meta.env.VITE_BACKEND_URL,
-   withCredentials: true,
+   baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:9008',
+   withCredentials: true, // Important if you rely on cookies for auth!
+});
+
+axiosInstance.interceptors.request.use((config) => {
+   // If you use JWT tokens stored in localStorage
+   const token = localStorage.getItem('jwt');
+   if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+   }
+   return config;
 });
 
 export default axiosInstance;
